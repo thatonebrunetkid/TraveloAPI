ec6bedc4e3827ccbb4c03153288e9f0731beb62d
Mateusz Augustyniak <s18531@pjwstk.edu.pl>
2022-09-19 23:38:20 +0200

    Metadata

---
+++ Application/DTOs/Payment/PaymentDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Application.DTOs.Payment
+{
+    public class PaymentDTO
+    {
+        public int UserID { get; set; }
+        public string TargetPhoneNumber { get; set; }
+        public string BlikCode { get; set; }
+        public bool CorrectionFlag { get; set; }
+    }
+}
+
---
+++ TraveloAPI/Controllers/PaymentController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.DTOs.Payment;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TraveloAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : Controller
+    {
+        [Route("Initialize")]
+        [HttpPost]
+        public async Task<ActionResult> AuthorisePayment([FromBody] PaymentDTO request) 
+        {
+            if (request.BlikCode == "" || request.TargetPhoneNumber == "") return BadRequest();
+
+            if (request.CorrectionFlag)
+                return Ok();
+            else
+                return StatusCode(500);
+        }
+
+    }
+}
+
