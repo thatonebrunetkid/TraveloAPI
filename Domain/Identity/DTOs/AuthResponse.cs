d96e067b8273f2343d4bbbf9740adeca65fcaf45
Mateusz Augustyniak <s18531@pjwstk.edu.pl>
2022-12-04 15:56:00 +0100

    Add additional project for authemtication

---
+++ Domain/Identity/DTOs/AuthResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Identity.DTOs
+{
+    public class AuthResponse
+    {
+        public string Id { get; set; }
+        public string Token { get; set; }
+    }
+}
